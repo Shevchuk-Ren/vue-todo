150f8868128a8cc2535ee3b7eb85fc19130b8e5e
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-11-15 14:11:29 +0200

    use persist

--- src/store/index.js
+++ src/store/index.js
@@ -48,7 +48,12 @@
 // })
 
 import { createStore } from 'vuex';
+import VuexPersistence from 'vuex-persist'
 import todos from './modules/Todos/todos';
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
 
 export default createStore({
   // state: {
@@ -120,4 +125,5 @@ export default createStore({
   //   // },
   // },
   modules: { todos },
+   plugins: [vuexLocal.plugin]
 });
